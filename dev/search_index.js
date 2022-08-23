d58f4ce82ffd9d1fb4cfc90a6640047aa70333ce
Documenter.jl <documenter@juliadocs.github.io>
2022-08-23 10:13:33 +0000

    build based on 1f547ba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PolynomialMatrixEquations.jl","page":"Home","title":"PolynomialMatrixEquations.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PolynomialMatrixEquations.jl","category":"page"}]
+}
