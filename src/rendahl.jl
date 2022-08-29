236c0b3b5a7c1d020a9dbad0d1bad98826aefdb1
louisponet <louisponet@gmail.com>
2022-08-29 20:23:19 +0200

    rendahl

--- src/rendahl.jl
+++ src/rendahl.jl
@@ -1,73 +1,63 @@
-function solve_system(A, B, C, linws, maxiter=1000, tol=1e-6)
+struct RendahlWs
+    A_tmp::Matrix{Float64}
+    B_tmp::Matrix{Float64}
+    C_tmp::Matrix{Float64}
+    S0::Matrix{Float64}
+    linws::LUWs
+end
+RendahlWs(n::Int) = RendahlWs(Matrix{Float64}(undef, n, n),
+                              Matrix{Float64}(undef, n, n),
+                              Matrix{Float64}(undef, n, n),
+                              Matrix{Float64}(undef, n, n),
+                              LUWs(n))
+                              
     
-    #==
-    Solves for P and Q using Rehndal's Algorithm
-    ==#
-    C_orig = copy(C)
-    B_orig = similar(B)
-    A_orig = copy(A)
-    C_buf = similar(C)
-    A_buf = similar(A)
-    B_buf = copy(B)
+function rendahl_solve!(X::Matrix, A::Matrix, B::Matrix, C::Matrix, ws::RendahlWs, maxiter=1000, tol=1e-6)
     
-    F0 = zero(A)
-    S0 = zero(A)
-    error = one(tol) + tol
+    err = one(tol) + tol
     iter = 0
-    
-    while error > tol && iter <= maxiter
-        copy!(B_buf, B)
-        mul!(B_buf, C, F0, 1.0, 1.0)
-        lu_t = LU(factorize!(linws, B_buf)...)
-        copy!(A_buf, A)
-        copy!(A_orig, A)
-        copy!(B_orig, B)
-        ldiv!(lu_t, A_buf)
-        lmul!(-1.0, A_buf)
-        F1 = A_buf
-        copy!(C_buf, C)
-        copy!(B_buf, B)
-        mul!(B_buf, A, S0, 1.0, 1.0)
-        # S1 = -(A * S0 + B) \ C
-        lu_t = LU(factorize!(linws, B_buf)...)
-        ldiv!(lu_t, C_buf)
-        lmul!(-1.0, C_buf)
-        S1 = C_buf
-        mul!(A_orig, B_orig, F1, 1.0, 1.0)
-        mul!(C_orig, F1, F1, 1.0, 0.0)
-        mul!(A_orig, C, C_orig, 1.0, 1.0)
+    fill!(ws.S0, 0.0)
+    fill!(X, 0.0)
+    while err > tol && iter <= maxiter
+        copy!(ws.B_tmp, B)
+        mul!(ws.B_tmp, C, X, 1.0, 1.0)
         
-        # error = maximum(C * F1 * F1  + B * F1 + A)
-        error = maximum(A_orig)
+        lu_t = LU(factorize!(ws.linws, ws.B_tmp)...)
+        copy!(ws.A_tmp, A)
+        ldiv!(lu_t, ws.A_tmp)
+        lmul!(-1.0, ws.A_tmp)
+        X .= ws.A_tmp
+
+        copy!(ws.C_tmp, C)
+        copy!(ws.B_tmp, B)
+        mul!(ws.B_tmp, A, ws.S0, 1.0, 1.0)
+
+        lu_t = LU(factorize!(ws.linws, ws.B_tmp)...)
+        ldiv!(lu_t, ws.C_tmp)
+        lmul!(-1.0, ws.C_tmp)
+        ws.S0 .= ws.C_tmp
         
-        F0 .= F1
-        S0 .= S1
-        
+        copy!(ws.B_tmp, A)
+        mul!(ws.B_tmp, B, X, 1.0, 1.0)
+        mul!(ws.A_tmp, X, X, 1.0, 0.0)
+        mul!(ws.B_tmp, C, ws.A_tmp, 1.0, 1.0)
+
+        err = maximum(ws.B_tmp)
         iter += 1
         
     end
-    # fill!(v, 0.0)
-    # v[2] = 1
-    # XP = power_eigenvalue(v, F0, v_buf,1e-2)
-    # fill!(v, 0.0)
-    # v[1] = 1
-    # XS = power_eigenvalue(v, S0, v_buf, 1e-2)
+    if iter > maxiter
+        XP = maximum(x->abs(x), eigvals(X))
+        XS = maximum(x->abs(x), eigvals(ws.S0))
     
-    # if iter == maxiter
-    #     outmessage = "Convergence Failed. Max Iterations Reached. Error: $error"
-    # elseif XP > 1.0
-    #     outmessage = "No Stable Solution Exists!"
-    # elseif XS > 1.0
-    #     outmessage = "Multiple Solutions Exist!"
-    # else
-    #     outmessage = "Convergence Successful!"
-    # end
-    
-    # Q = -(C * F0 + B) \ E
-
-
-    return F0
-
-    
+        if XP > 1.0
+            throw(UnstableSystemException())
+        elseif XS > 1.0
+            throw(UndeterminateSystemException())
+        else
+            error("System not converged.")
+        end
+    end 
+    return X
 end
 
